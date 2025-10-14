8bbafe6f6f3db7da50287104c64ab52d152d2a72
Raymundo <meesra0201@gmail.com>
2025-10-13 21:00:55 -0600

    Preparando Deploy Se corre collectstatic y se crea archivo de requerimientos

--- miWeb/settings.py
+++ miWeb/settings.py
@@ -13,23 +13,17 @@ import os
 from pathlib import Path
 from environs import env
 
-env.read_env(".env_")  # read .env file, if it exists
+env.read_env(".env")  # read .env file, if it exists
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+DEBUG = os.environ.get('DEBUG')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2f(g@$*85-+@$p$t47a=dkrbvnm(%9q%hbl+ppkz^*09&!$-r('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -154,6 +148,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "archivosEstaticos"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
