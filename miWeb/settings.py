1aa48fcbfc74ddba6c859344477194afddb28929
Raymundo <meesra0201@gmail.com>
2025-10-14 11:44:42 -0600

    Preparando deploy

--- miWeb/settings.py
+++ miWeb/settings.py
@@ -114,6 +114,25 @@ else:
         }
     }
 
+if ENVIRONMENT == 'development':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("DB_NAME"),
+            'USER': os.environ.get("DB_USER"),
+            'PASSWORD': os.environ.get("DB_PASSWORD"),
+            'HOST': os.environ.get("DB_HOST"),
+            'PORT': os.environ.get("DB_PORT", '5432'),
+            'CONN_TOUT': 900,
+        }
+    }
+else: 
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+
+
 #print(DATABASES)
 
 
